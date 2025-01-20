7cf1bd20f994e58908a85633904e80a3e287ef93
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-01-20 10:49:28 +0530

    Create literals in Java.java

---
+++ literals in Java.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 0b101;
+        System.out.println(num1);
+        double num2 = 10_00_00_000;
+        System.out.println(num2);
+        }
+
+    }
