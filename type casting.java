05a600a3ab27d6d87dd543cc27cd81f061161366
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-01-20 10:53:39 +0530

    Create type casting.java

---
+++ type casting.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte b = 123;
+        int a = 256;
+        float f = 2.6f;
+        int x = (int)f;
+
+
+
+
+
+
+        }
+
+    }
