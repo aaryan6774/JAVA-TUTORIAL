8cfdc7e2077b8b4c90fba64c8fe1e070e823b504
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-01-20 11:03:35 +0530

    Create Relations.java

---
+++ Relations.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 5;
+        boolean result = x>y;
+        System.out.println(result);
+        System.out.println(result);
+        System.out.println(result);
+
+
+        }
+
+    }
