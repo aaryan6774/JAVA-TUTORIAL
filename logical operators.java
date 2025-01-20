f7e091c76a4f5295b6e3b77015040090f38b0119
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-01-20 11:12:29 +0530

    Create logical operators.java

---
+++ logical operators.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        Logical Operator
+        int x = 7;
+        int y = 5;
+        int a = 5;
+        int b = 9;
+        boolean result = x > y   ||  a > b;
+        System.out.println(result);
+
+
+
+
+        }
+
+
+    }
