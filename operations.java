a1a3e562465b8c495768798ebdca9ef1f731c698
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-01-20 10:59:22 +0530

    Create operations.java

---
+++ operations.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 5;
+        int res = num1 % num2;
+        System.out.println(res);
+        int i = 2;
+        i+=1;
+        i+=8;
+        i+=10;
+        System.out.println(i);
+        int n = 1;
+        int result = ++n;
+        System.out.println(result);
+
+
+
+        }
+
+    }
