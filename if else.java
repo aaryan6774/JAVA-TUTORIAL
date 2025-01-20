ae1b9fbd9709f17d4653cfd6fcf0cc9b4da1513a
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-01-20 11:16:10 +0530

    Create if else.java

---
+++ if else.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 0;
+        if(x > 12)
+        {
+            System.out.println("Hello");
+        } else if (x == 0) {
+            System.out.println("not so good");
+
+        }
+        else
+        {
+            System.out.println("Hi");
+        }
+    }
+}
