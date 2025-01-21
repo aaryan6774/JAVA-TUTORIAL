e0c1ccc1e896b3475ff0c637c1f44ed4a1cbdd07
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-01-21 10:18:16 +0530

    Create Static Variable in Java.java

---
+++ Static Variable in Java.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Mobile
+{
+    String brand;
+    int price;
+    String name;
+}
+public class Main {
+    public static void main(String[] args) {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 150000;
+        obj1.name = "SmartPhone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Apple";
+        obj2.price = 150000;
+        obj2.name = "SmartPhone";
+        
+
+    }
+}
